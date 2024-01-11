e3d4992daa7c9dc84d5b9b00002ccaeacc7110b8
satishchandra.d <satishchandra.d@celestialsys.com>
2024-01-11 23:41:40 +0530

    Powerpoint Parser

---
+++ SimplePowerpointParserApplication/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace SimplePowerpointParserApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TBSourceFile = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TBOutputFile = new System.Windows.Forms.TextBox();
+            this.BtnBrowsePPT = new System.Windows.Forms.Button();
+            this.BtnBrouwseOutput = new System.Windows.Forms.Button();
+            this.BtnOk = new System.Windows.Forms.Button();
+            this.BtnCancel = new System.Windows.Forms.Button();
+            this.BtnParsePPT = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(129, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "WELCOME to Powerpoint Parser";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Source File";
+            // 
+            // TBSourceFile
+            // 
+            this.TBSourceFile.Location = new System.Drawing.Point(27, 43);
+            this.TBSourceFile.Name = "TBSourceFile";
+            this.TBSourceFile.Size = new System.Drawing.Size(232, 20);
+            this.TBSourceFile.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Output File";
+            // 
+            // TBOutputFile
+            // 
+            this.TBOutputFile.Location = new System.Drawing.Point(27, 100);
+            this.TBOutputFile.Name = "TBOutputFile";
+            this.TBOutputFile.Size = new System.Drawing.Size(232, 20);
+            this.TBOutputFile.TabIndex = 4;
+            // 
+            // BtnBrowsePPT
+            // 
+            this.BtnBrowsePPT.Location = new System.Drawing.Point(280, 40);
+            this.BtnBrowsePPT.Name = "BtnBrowsePPT";
+            this.BtnBrowsePPT.Size = new System.Drawing.Size(29, 23);
+            this.BtnBrowsePPT.TabIndex = 5;
+            this.BtnBrowsePPT.Text = "...";
+            this.BtnBrowsePPT.UseVisualStyleBackColor = true;
+            this.BtnBrowsePPT.Click += new System.EventHandler(this.BtnBrowsePPT_Click);
+            // 
+            // BtnBrouwseOutput
+            // 
+            this.BtnBrouwseOutput.Location = new System.Drawing.Point(280, 97);
+            this.BtnBrouwseOutput.Name = "BtnBrouwseOutput";
+            this.BtnBrouwseOutput.Size = new System.Drawing.Size(29, 23);
+            this.BtnBrouwseOutput.TabIndex = 6;
+            this.BtnBrouwseOutput.Text = "...";
+            this.BtnBrouwseOutput.UseVisualStyleBackColor = true;
+            this.BtnBrouwseOutput.Click += new System.EventHandler(this.BtnBrouwseOutput_Click);
+            // 
+            // BtnOk
+            // 
+            this.BtnOk.Location = new System.Drawing.Point(217, 251);
+            this.BtnOk.Name = "BtnOk";
+            this.BtnOk.Size = new System.Drawing.Size(75, 23);
+            this.BtnOk.TabIndex = 7;
+            this.BtnOk.Text = "OK";
+            this.BtnOk.UseVisualStyleBackColor = true;
+            this.BtnOk.Click += new System.EventHandler(this.BtnOK_Click);
+            // 
+            // BtnCancel
+            // 
+            this.BtnCancel.Location = new System.Drawing.Point(308, 251);
+            this.BtnCancel.Name = "BtnCancel";
+            this.BtnCancel.Size = new System.Drawing.Size(75, 23);
+            this.BtnCancel.TabIndex = 8;
+            this.BtnCancel.Text = "Cancel";
+            this.BtnCancel.UseVisualStyleBackColor = true;
+            this.BtnCancel.Click += new System.EventHandler(this.BtnCancel_Click);
+            // 
+            // BtnParsePPT
+            // 
+            this.BtnParsePPT.Location = new System.Drawing.Point(105, 143);
+            this.BtnParsePPT.Name = "BtnParsePPT";
+            this.BtnParsePPT.Size = new System.Drawing.Size(75, 23);
+            this.BtnParsePPT.TabIndex = 9;
+            this.BtnParsePPT.Text = "Parse PPT";
+            this.BtnParsePPT.UseVisualStyleBackColor = true;
+            this.BtnParsePPT.Click += new System.EventHandler(this.BtnParsePPT_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.BtnParsePPT);
+            this.groupBox1.Controls.Add(this.TBSourceFile);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.TBOutputFile);
+            this.groupBox1.Controls.Add(this.BtnBrouwseOutput);
+            this.groupBox1.Controls.Add(this.BtnBrowsePPT);
+            this.groupBox1.Location = new System.Drawing.Point(27, 54);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(356, 176);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Configuration";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 290);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.BtnCancel);
+            this.Controls.Add(this.BtnOk);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TBSourceFile;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TBOutputFile;
+        private System.Windows.Forms.Button BtnBrowsePPT;
+        private System.Windows.Forms.Button BtnBrouwseOutput;
+        private System.Windows.Forms.Button BtnOk;
+        private System.Windows.Forms.Button BtnCancel;
+        private System.Windows.Forms.Button BtnParsePPT;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ SimplePowerpointParserApplication/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Core;
+using System.Xml;
+using Microsoft.Office.Interop.PowerPoint;
+using Application = Microsoft.Office.Interop.PowerPoint.Application;
+using Shape = Microsoft.Office.Interop.PowerPoint.Shape;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace SimplePowerpointParserApplication
+{
+    public class SlideData
+    {
+        public int SlideIndex { get; set; }
+        public List<ShapeData> ShapeDataList { get; set; }
+    }
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnOK_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BtnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BtnParsePPT_Click(object sender, EventArgs e)
+        {
+            // Get input file path from the text box
+            string inputFilePath = TBSourceFile.Text;
+
+            // Get output file path from the text box
+            string outputFilePath = TBOutputFile.Text;
+
+            if (string.IsNullOrEmpty(inputFilePath) || string.IsNullOrEmpty(outputFilePath))
+            {
+                MessageBox.Show("Please enter both input and output file paths.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Create an instance of PowerPoint Application
+            Application powerPointApp = new Application();
+
+            // Open the PowerPoint presentation
+            Presentation presentation = powerPointApp.Presentations.Open(inputFilePath, MsoTriState.msoFalse, MsoTriState.msoFalse, MsoTriState.msoFalse);
+
+            // Create a list to store slide data
+            List<SlideData> slideDataList = new List<SlideData>();
+
+            // Iterate through each slide and extract text
+            foreach (Slide slide in presentation.Slides)
+            {
+                // Create a list to store shape data
+                List<ShapeData> shapeDataList = new List<ShapeData>();
+
+                // Iterate through each shape in the slide
+                foreach (Shape shape in slide.Shapes)
+                {
+                    // Extract shape information (type, coordinates, text)
+                    ShapeData shapeData = new ShapeData
+                    {
+                        ShapeType = shape.AutoShapeType.ToString(),
+                        Coordinates = new Rect
+                        {
+                            Left = shape.Left,
+                            Top = shape.Top,
+                            Width = shape.Width,
+                            Height = shape.Height
+                        },
+                        Text = shape.TextFrame.TextRange.Text
+                    };
+
+                    // Add shape data to the list
+                    shapeDataList.Add(shapeData);
+                }
+
+                // Add slide data to the list
+                slideDataList.Add(new SlideData
+                {
+                    SlideIndex = slide.SlideIndex,
+                    ShapeDataList = shapeDataList
+                });
+            }
+
+            // Close the PowerPoint presentation
+            presentation.Close();
+
+            // Quit PowerPoint application
+            powerPointApp.Quit();
+
+            // Convert slide data to JSON format
+            string jsonData = JsonConvert.SerializeObject(slideDataList, Newtonsoft.Json.Formatting.Indented);
+
+            try
+            {
+                // Save JSON data to the specified output file
+                File.WriteAllText(outputFilePath, jsonData);
+
+                MessageBox.Show("Parsing completed and JSON data saved successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error saving JSON data: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void BtnBrowsePPT_Click(object sender, EventArgs e)
+        {
+            // Show the file dialog to select a PowerPoint file
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "PowerPoint Files|*.pptx;*.ppt",
+                Title = "Select a PowerPoint File"
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Set the selected file path to the input file text box
+                TBSourceFile.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void BtnBrouwseOutput_Click(object sender, EventArgs e)
+        {
+            // Show the folder dialog to select the output folder
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog
+            {
+                Description = "Select the Output Folder"
+            };
+
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Set the selected folder path to the output folder text box
+                TBOutputFile.Text = folderBrowserDialog.SelectedPath + "\\Output.JSON";
+            }
+        }
+    }
+    public class ShapeData
+    {
+        public string ShapeType { get; set; }
+        public Rect Coordinates { get; set; }
+        public string Text { get; set; }
+    }
+
+    public class Rect
+    {
+        public double Left { get; set; }
+        public double Top { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+    }
+}
